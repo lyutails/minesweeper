c7359a162364b965d72e96b16b8d06d22df3bbc8
lyutails <lyusya.cool@gmail.com>
2023-05-16 03:00:42 +0300

    feat: create flags data array

---
+++ src/data/data_flags.js
@@ -0,0 +1,44 @@
+export const flagsData = [
+    {
+        pic: '../refs/icons/apple.svg'
+    },
+    {
+        pic: '../refs/icons/blueberries.svg'
+    },
+    {
+        pic: '../refs/icons/cherry.svg'
+    },
+    {
+        pic: '../refs/icons/cocktail.svg'
+    },
+    {
+        pic: '../refs/icons/cupcake.svg'
+    },
+    {
+        pic: '../refs/icons/grapes.svg'
+    },
+    {
+        pic: '../refs/icons/macaron.svg'
+    },
+    {
+        pic: '../refs/icons/mango.svg'
+    },
+    {
+        pic: '../refs/icons/peach.svg'
+    },
+    {
+        pic: '../refs/icons/raspberry.svg'
+    },
+    {
+        pic: '../refs/icons/strawberry.svg'
+    },
+    {
+        pic: '../refs/icons/sushi.svg'
+    },
+    {
+        pic: '../refs/icons/tomato.svg'
+    },
+    {
+        pic: '../refs/icons/watermelon.svg'
+    },
+]
