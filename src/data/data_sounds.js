997a36cba20ac6c35818ada7cc25be7d6970f400
lyutails <lyusya.cool@gmail.com>
2023-05-22 12:59:15 +0300

    feat: create sounds data array

---
+++ src/data/data_sounds.js
@@ -0,0 +1,35 @@
+import lose from "../refs/icons/flags/063_lose.m4a";
+import win from "../refs/icons/flags/066_win.svg";
+import bet from "../refs/icons/flags/068_bet.svg";
+import bet from "../refs/icons/flags/068_bet.svg";
+import bet from "../refs/icons/flags/068_bet.svg";
+import bet from "../refs/icons/flags/068_bet.svg";
+import bet from "../refs/icons/flags/068_bet.svg";
+import bet from "../refs/icons/flags/068_bet.svg";
+
+export const flagsData = [
+  {
+    pic: lose,
+  },
+  {
+    pic: win,
+  },
+  {
+    pic: bet,
+  },
+  {
+    pic: bet,
+  },
+  {
+    pic: bet,
+  },
+  {
+    pic: bet,
+  },
+  {
+    pic: bet,
+  },
+  {
+    pic: bet,
+  },
+];
